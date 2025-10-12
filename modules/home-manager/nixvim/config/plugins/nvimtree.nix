6935afe12855dddf35faaf984f0106723211ecf1
Estikno <95084542+Estikno@users.noreply.github.com>
2025-10-12 16:21:27 +0200

    Add custom nvimtree keymaps

--- modules/home-manager/nixvim/config/plugins/nvimtree.nix
+++ modules/home-manager/nixvim/config/plugins/nvimtree.nix
@@ -47,6 +47,29 @@
           };
         };
       };
+      on_attach.__raw = ''
+        function(bufnr)
+          local api = require("nvim-tree.api")
+
+          local function opts(desc)
+            return {
+              desc = "nvim-tree: " .. desc,
+              buffer = bufnr,
+              noremap = true,
+              silent = true,
+              nowait = true
+            }
+          end
+
+          -- load default mappings
+          api.config.mappings.default_on_attach(bufnr)
+
+          -- custom ones
+          vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+          vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+        end
+      '';
     };
+
   };
 }
