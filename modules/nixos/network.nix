be5f5544d61db04e7e84e645aef0236c21b2e317
Estikno <95084542+Estikno@users.noreply.github.com>
2025-11-25 18:39:25 +0100

    Fix spelling error

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -8,6 +8,6 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
+  # Enable NetworkManager
   networking.networkmanager.enable = true;
 }
